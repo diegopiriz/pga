1c4798f64b2be3d85a36717161013b47cad49e7d
Diego Piriz <diego.pirizbruny@gmail.com>
2014-03-04 23:35:26 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pga::Application.config.secret_key_base = '281ea283e3e1b5c20f63089460cee1a07a248c9301b2205f3b6506b39ef89d587327428c12582f585cc720f6d06131cff2eae1b2c8df32faee45a73dc6b3b330'
