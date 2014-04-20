62fe3be52d052a4187079ebb0ce2ffd3993d97c7
Diego Piriz <diego.pirizbruny@gmail.com>
2014-04-20 19:01:00 -0300

    new migrations

--- db/migrate/20140305014448_create_dog_pictures.rb
+++ db/migrate/20140305014448_create_dog_pictures.rb
@@ -3,8 +3,6 @@ class CreateDogPictures < ActiveRecord::Migration
     create_table :dog_pictures do |t|
       t.integer :dog_id
       t.binary :data
-      t.string :mime_type
-      t.string :filename
 
       t.timestamps
     end
--- db/migrate/20140415060346_remove_mime_type_and_file_name_from_dog_pictures.rb
+++
@@ -1,6 +0,0 @@
-class RemoveMimeTypeAndFileNameFromDogPicture < ActiveRecord::Migration
-  def change
-    remove_column  :dog_picture, :mime_type
-    remove_column  :dog_picture, :filename
-  end
-end
